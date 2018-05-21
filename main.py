739e80e7ea9e0e59c2f9a0240d2565f6a06d1411
kevin <kevifunau@gmail.com>
2018-05-21 22:20:35 +1000

    5.21 kai

---
+++ main.py
@@ -0,0 +1,57 @@
+
+
+"""
+comp9417 project 
+Topic name : Recommender system using collaborative filtering@author: Jiarui Ding (z5045636)
+@author: Kai Fang (z5137591)
+"""
+
+####### import modules ########
+
+
+import numpy as numpy
+
+
+###########
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############    main #################
+
+if __name__ == '__main__':
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
